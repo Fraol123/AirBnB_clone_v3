c1d4a6b59dee5d98c5cc3ca3b76973267cd22630
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-19 09:44:36 +0300

    json dict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,28 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
 @app_views.route('/status',strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
 
 
 
