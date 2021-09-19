6f0bdd4851d4ac8c47c1a3db7418bdcfd35b1911
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-19 13:37:24 +0300

    state route created

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ from flask import Blueprint
 #create a variable app_views which is an instance of Blueprint (url prefix must be /api/v1)
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
+from api.v1.views.states import *
