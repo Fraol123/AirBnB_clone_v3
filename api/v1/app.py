5f08f0ff3f77083877e53a0060404ca56ecb4cee
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-20 08:53:38 +0300

    pep8 check

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,26 +7,26 @@ from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
-
 app = Flask('app')
-#register the blueprint app_views to your Flask instance app
+# register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
-#declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
 def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
-    #run your Flask server (variable app) with:
-    #host = environment variable HBNB_API_HOST or 0.0.0.0 as default value
-    #port = environment variable HBNB_API_PORT or 5000 as default value
+    # run your Flask server (variable app) with:
+    # host = environment variable HBNB_API_HOST or 0.0.0.0 as default value
+    # port = environment variable HBNB_API_PORT or 5000 as default value
     app.run(host=os.getenv('HBNH_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)))
